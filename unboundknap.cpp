3359b8ef7170be79f2de3f893d7b728570c8ebcf
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-07 11:07:14 +0530

    Update unboundknap.cpp

--- unboundknap.cpp
+++ unboundknap.cpp
@@ -1,34 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int dp[1001][1001];
 int max(int a, int b) { return (a > b) ? a : b; }
-int knap1(int wt[],int val[],int sum,int n)
+int un_knap1(int wt[],int v[],int W,int n)
 {
-    
-       int dp[n+1][sum+1];
-       
-       for(int i=0; i<n+1; i++){
-           for(int j=0; j<sum+1; j++){
-               if(!i||!j){
-                   dp[i][j]=0;
-               }
-               else if(wt[i-1]<=j){
-                   dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]], dp[i-1][j]);
-               }
-               else{
-                  dp[i][j]=dp[i-1][j]; 
-               }
-           }
-       }
-       
-       return dp[n][sum];
+    for(int i=0;i<=n;i++)
+    for(int j=0;j<=W;j++)
+    if(i==0||j==0)
+    dp[i][j]=0;
+     else if(wt[i-1]>j)
+        dp[i][j]=dp[i-1][j];
+    else 
+        dp[i][j]=max(v[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+        return dp[n][W];
 }
 int main()
-{  
+{   memset(dp,-1,sizeof(dp));
   
     int n,W;cin>>n>>W;int wt[n],v[n];
-  
+     for(int i=0;i<=n;i++)dp[i][0]=0,dp[0][i]=0;
     for(int i=0;i<n;i++)cin>>wt[i];
     for(int i=0;i<n;i++)cin>>v[i];
-    cout<<knap1(wt,v,W,n);
+    cout<<un_knap1(wt,v,W,n);
 }
