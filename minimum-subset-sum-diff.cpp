7b5325d80256cb72939b470f437d10bbe93be97d
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-07 19:33:14 +0530

    Update minimum-subset-sum-diff.cpp

--- minimum-subset-sum-diff.cpp
+++ minimum-subset-sum-diff.cpp
@@ -1,25 +1,61 @@
-#include<bits/stdc++.h>
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
-bool dp[1001][1001];
-int max(int a, int b) { return (a > b) ? a : b; }
-void knap1(int wt[],int W,int n)
+
+ // } Driver Code Ends
+class Solution{
+    public: 
+    int knap(int a[],int n,int W){
+        int min=INT_MAX;
+        bool dp[n+1][W+1];
+        for(int i=0;i<=n;i++)
+        for(int j=0;j<=W+1;j++)
+         if(j==0)dp[i][j]=true;
+         else if(i==0)dp[i][j]=false;
+         else if(a[i-1]>j)
+         dp[i][j]=dp[i-1][j];
+         else dp[i][j]=dp[i-1][j-a[i-1]]||dp[i-1][j];
+        
+        for(int i=1;i<=W;i++)
+            {
+                if(dp[n][i])
+                {int a1=abs(2*i-W);
+                if(a1<min)min=a1;}
+            } 
+            return min;
+}
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int sum=0;
+	    for(int i=0;i<n;i++)sum+=arr[i];
+	    return knap(arr,n,sum);
+	} 
+};
+
+
+// { Driver Code Starts.
+int main() 
 {
-    for(int i=1;i<=n;i++)
-    for(int j=1;j<=W;j++)
-    if(wt[i-1]>j)
-        dp[i][j]=dp[i-1][j];
-    else 
-        dp[i][j]= dp[i-1][j-wt[i-1]]||dp[i-1][j];
-    
-}
-int main()
-{   
-    
-    int n,W=0,ans=INT_MAX;cin>>n;int wt[n],v[n];
-    for(int i=0;i<=n;i++)dp[i][0]=true;
-    for(int i=0;i<n;i++)cin>>wt[i],W+=wt[i];
-    for(int i=1;i<=W;i++){int cur;
-        if(dp[n][i])cur=abs(W-2*i);
-        ans=min(cur,ans);
-    }cout<<ans<<" ";
-}
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends
