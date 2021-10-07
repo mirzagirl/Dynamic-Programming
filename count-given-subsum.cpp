0e2db77c4edb69a56d5a15be408d476bdeb2f0a3
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-07 19:14:59 +0530

    Update count-given-subsum.cpp

--- count-given-subsum.cpp
+++ count-given-subsum.cpp
@@ -1,23 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-int dp[1001][1001]={0};
-int max(int a, int b) { return (a > b) ? a : b; }
-int knap1(int wt[],int W,int n)
-{
-    for(int i=1;i<=n;i++)
-    for(int j=1;j<=W;j++)
-    if(wt[i-1]>j)
-        dp[i][j]=dp[i-1][j];
-    else 
-        dp[i][j]= dp[i-1][j-wt[i-1]]+dp[i-1][j];
-    return dp[n][W];
-}
-int main()
-{   
-    
-    int n,W;cin>>n>>W;int wt[n],v[n];
-    for(int i=0;i<=n;i++)dp[i][0]=1;
-    for(int i=0;i<n;i++)cin>>wt[i];
-    // for(int i=0;i<n;i++)cin>>v[i];
-    cout<<knap1(wt,W,n);
-}
+ vector<vector<int>> t(n+1,vector<int> (sum+1));
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<sum+1; j++){
+                
+                if(j==0)   t[i][j]=1;
+               else if(i==0)    t[i][j]=0;
+               
+               else if(arr[i-1] > j)    t[i][j]=t[i-1][j]%1000000007;
+               
+               else         t[i][j]=t[i-1][j]%1000000007 + t[i-1][j-arr[i-1]]%1000000007;
+            }
+        }
+        return (int)t[n][sum]%1000000007;
