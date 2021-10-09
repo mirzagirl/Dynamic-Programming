d82a832092ea7f3a8955ad9ac9508fb97199efc5
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-09 18:05:42 +0530

    Create long-com-subse-memoiza.cpp

---
+++ long-com-subse-memoiza.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+ // } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:  int dp[1001][1001];
+   int func(int x, int y, string &s1, string &s2){
+
+    
+       if(x==0 || y==0)return 0;
+       if(dp[x][y]!=-1)return dp[x][y];
+       if(s1[x-1]==s2[y-1]){
+           return dp[x][y]=(1+func(x-1,y-1,s1,s2));
+       }
+       else{
+           return dp[x][y]=max(func(x-1,y,s1,s2),func(x,y-1,s1,s2));   
+       }   
+   }
+
+
+    public:
+  
+    int lcs(int x, int y, string s1, string s2)
+    {
+      if(x==0 || y==0)return 0;
+       memset(dp,-1,sizeof dp);
+       return func(x,y,s1,s2);
+    }
+         
+    
+        // your code here
+    
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t,n,k,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(x, y, s1, s2) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
