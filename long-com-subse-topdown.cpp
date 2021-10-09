05647234d0b74de3c7be6b1f6f931e782344266c
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-09 18:43:49 +0530

    Update and rename long-com-subse-recur.cpp to long-com-subse-topdown.cpp

--- long-com-subse-topdown.cpp
+++ long-com-subse-topdown.cpp
@@ -7,18 +7,34 @@ using namespace std;
 
 class Solution
 {
+    public:  int dp[1001][1001];
+   int func(int x, int y, string &s1, string &s2){
+           int n=s1.size();
+           int m=s2.size();
+       for(int i=0;i<=n;i++)dp[i][0]=0;
+       for(int j=0;j<=m;j++)dp[0][j]=0;
+       for(x=1;x<=n;x++)
+       for(y=1;y<=m;y++)
+        if(s1[x-1]==s2[y-1])
+            dp[x][y]=1+dp[x-1][y-1];
+       else
+            dp[x][y]=max(dp[x-1][y],dp[x][y-1]);   
+         return dp[n][m];
+   }
+
+
     public:
-    //Function to find the length of longest common subsequence in two strings.
+  
     int lcs(int x, int y, string s1, string s2)
     {
-        
-        if(x==0||y==0)return 0;
-        else if(s1[x-1]==s2[y-1])
-        return 1+lcs(x-1,y-1,s1,s2);
-        else
-          return max(lcs(x-1,y,s1,s2),lcs(x,y-1,s1,s2));
-         
+      if(x==0 || y==0)return 0;
+       memset(dp,-1,sizeof dp);
+       return func(x,y,s1,s2);
     }
+         
+    
+        // your code here
+    
 };
 
 
