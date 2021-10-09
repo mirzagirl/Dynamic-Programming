d27317596974ebaa4a8493cd955e02ff7c2e0c7b
mirzagirl <61827509+mirzagirl@users.noreply.github.com>
2021-10-09 23:11:15 +0530

    Update printting-subseque.cpp

--- printting-subseque.cpp
+++ printting-subseque.cpp
@@ -1,61 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-
- // } Driver Code Ends
-class Solution{
-    public:
-      public:  int dp[1001][1001];
-   string func(int x, int y, string &s1, string &s2){
-           int n=s1.size();
-           int m=s2.size();
-       for(int i=0;i<=n;i++)dp[i][0]=0;
-       for(int j=0;j<=m;j++)dp[0][j]=0;
-       for(x=1;x<=n;x++)
-       for(y=1;y<=m;y++)
-        if(s1[x-1]==s2[y-1])
-            dp[x][y]=1+dp[x-1][y-1];
-       else
-            dp[x][y]=0;  
-          string s="";
-          int i=n;int j=m;
-         
-          while(i>0&&j>0){
-              
-              if(dp[i-1][j]==dp[i][j-1])
-              {
-                  i--;j--;s+=s1[i-1];
-                  
-              }
-              else{
-                  if(dp[i-1][j]>dp[i][j-1])
-                      i--;
-                else j--;
-              }
-          }
-          if(s.size()==0)return "-1";
-          else return s;
-        
-   }
-    
-   string longestCommonSubstr (string S1, string S2, int n, int m)
-    {
-       
-       return func(n,m,S1,S2);
+class Solution {
+public:
+    int longestCommonSubsequence(string x, string y) {
+        int n=x.size(),m=y.size();
+        string s;
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        // for printing longest common subsequence
+        int i=n,j=m;
+        while(i>0 && j>0){    // if any of this i and j becomes 0 means one string is empty 
+            if(x[i-1]==y[j-1]){
+                s+=x[i-1];    //if character matches then add that to result string and move diagonally up
+                i--;j--;
+            }
+            else{
+                if(dp[i][j-1]>dp[i-1][j])j--;    // else find the max of up and right position of dp table and move accordingly to up or left
+                else i--;
+            }
+        }
+        reverse (s.begin(),s.end());    // reverse the string as we started from last
+        cout<<s<<"  AND SIZE = ";
+        return dp[n][m];
     }
 };
-
-// { Driver Code Starts.
-
 int main()
 {
     int t; cin >> t;
     while (t--)
     {
-        int n, m; cin >> n >> m;
+        
         string s1, s2;
         cin >> s1 >> s2;
         Solution ob;
 
-        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+        cout << ob.longestCommonSubsequence (s1, s2) << endl;
     }
 }
